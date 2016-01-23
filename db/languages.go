0be0832355d59cafe5fb8627585feb1bf95b2f56
freeeve <eve.freema@gmail.com>
2016-01-23 15:48:07 -0500

    sorting languages

--- db/languages.go
+++ db/languages.go
@@ -1,7 +1,7 @@
 package db
 
 func FindLanguages() ([]string, error) {
-	query := "select name from gophergala_languages"
+	query := "select name from gophergala_languages order by name"
 	langs := []string{}
 	err := db.Select(&langs, query)
 	if err != nil {
