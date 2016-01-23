b231709eeac601fafe13cb15a3fd86a446f90635
gophergala <gophergala@neo4j.skeweredrook.com>
2016-01-23 03:41:31 -0500

    fixing deploy script

--- pulldeploy.sh
+++ pulldeploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /home/gophergala/src/github.com/gophergala2016/wwcdc_01/
+cd /home/gophergala/gocode/src/github.com/gophergala2016/wwcdc_01/
 x=`git pull origin master | grep Already | wc -l`
-if [x -eq 1] then
+if [ $x -eq "1" ] 
+then
   echo "up to date"
 else 
   restart.sh
