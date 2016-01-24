4385e22422d908ab2849332ce97c4f3ca0bc06ab
aliaward <aliaward@gmail.com>
2016-01-23 23:16:32 -0500

    Update login.js
    updating login stuff

--- public/src/components/login/login.js
+++ public/src/components/login/login.js
@@ -3,28 +3,28 @@ import Map from 'can/map/';
 import 'can/map/define/';
 import './login.less!';
 import template from './login.stache!';
-//import LearningResources from 'gophergala/models/learning-resources';
+import userLogin from 'gophergala/models/login';
 
 export const ViewModel = Map.extend({
   define:{
     message: {
   //    value: {},
-  //    Type: LearningResources
-      value: 'This is the login component'
+      value: 'This is the login component',
+      Type: userLogin
     }
   },
-/*  send(event) {
+  send(event) {
     event.preventDefault();
-    let resource = this.attr('learningResource').attr();
-    console.log(resource)
-    new LearningResources(resource).save().then(() => {
-      console.log('hellooo')
+    let user = this.attr('userLogin').attr();
+    console.log(user)
+    new userLogin(user).save().then(() => {
+      console.log('login hellooo')
       this.resetValues()
     });
-  }, */
-//  resetValues() {
-//    this.attr('userLogin', {});
-//  }
+  }, 
+  resetValues() {
+    this.attr('userLogin', {});
+  }
 });
 
 export default Component.extend({
