3844460805691aacc7b7dc7d44161e1d0f9640fa
aliaward <aliaward@gmail.com>
2016-01-23 22:53:45 -0500

    Create login.js
    updating login stuff

---
+++ public/src/models/login.js
@@ -0,0 +1,31 @@
+import can from 'can';
+import superMap from 'can-connect/can/super-map/';
+import tag from 'can-connect/can/tag/';
+import 'can/map/define/define';
+
+export const userLogin = can.Map.extend({
+  define: {
+    username: {
+      type: 'string'
+    },
+    password: {
+      type: 'string'
+    }
+  }
+});
+
+//LearningResources.List = can.List.extend({
+//  Map: LearningResources
+//}, {});
+
+export const loginConnection = superMap({
+  url: '/api/user-auth',
+  idProp: 'id',
+  //Map: LearningResources,
+  //List: LearningResources.List,
+  name: 'login'
+});
+
+tag('login-model', loginConnection);
+
+//export default LearningResources;
