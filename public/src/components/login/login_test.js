9ed863d31e5e1f052362c15a11aa1f253ab828cd
aliaward <aliaward@gmail.com>
2016-01-23 20:25:06 -0500

    Update login_test.js
    updating login stuff

--- public/src/components/login/login_test.js
+++ public/src/components/login/login_test.js
@@ -1,10 +1,10 @@
 import QUnit from 'steal-qunit';
-import { ViewModel } from './add-resource';
+import { ViewModel } from './login';
 
 // ViewModel unit tests
-QUnit.module('gophergala/components/add-resource');
+QUnit.module('gophergala/components/login');
 
 QUnit.test('Has message', function(){
   var vm = new ViewModel();
-  QUnit.equal(vm.attr('message'), 'This is the add-resource component');
+  QUnit.equal(vm.attr('message'), 'This is the login component');
 });
